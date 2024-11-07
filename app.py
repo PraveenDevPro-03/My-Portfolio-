f81a1ca2044b524e861a069c390993ed507b9402
PraveenDevPro03 <Praveendiet33@gmail.com>
2024-11-07 12:10:26 +0530

    requirements.txt file adding

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template
+import os
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,4 @@ def home():
     return  render_template('index.html')
 
 if __name__=='__main__':
-    app.run(debug=True)
+ app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
