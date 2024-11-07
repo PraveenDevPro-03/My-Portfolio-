74e81bc545428c4b7ac398191a7e1f2c195869d3
PraveenDevPro03 <Praveendiet33@gmail.com>
2024-11-07 10:44:57 +0530

    My Portfolio uploading

---
+++ static/Js/main.js
@@ -0,0 +1,27 @@
+var tablinks=document.getElementsByClassName('tab-links')
+var tabcontents=document.getElementsByClassName('tab-contents')
+
+function opentab(tabname){
+    for(tablink of tablinks)
+    {
+        tablink.classList.remove('active-link');
+    }
+    for(tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab')
+}
+
+
+
+function toggleMenu() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+}
+
+
+
+
+
