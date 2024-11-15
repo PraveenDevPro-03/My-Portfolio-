235c8675ae80ad0305dcc4e2c534ac0873b024b7
PraveenDevPro03 <Praveendiet33@gmail.com>
2024-11-15 13:19:33 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask,render_template
-<<<<<<< HEAD
+from flask import Flask, render_template
 import os
 
-=======
->>>>>>> 91bf385 (updating resume)
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return  render_template('index.html')
+    return render_template("index.html")
 
-if __name__=='__main__':
-<<<<<<< HEAD
- app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-=======
-    app.run(debug=True,port=5050)
->>>>>>> 91bf385 (updating resume)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=5050)
