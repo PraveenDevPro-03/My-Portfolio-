997b08389a1e987dab299f7c55ec4f967cdb26ea
PraveenDevPro03 <Praveendiet33@gmail.com>
2024-11-15 12:47:54 +0530

    updating resume

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
+<<<<<<< HEAD
 import os
 
+=======
+>>>>>>> 91bf385 (updating resume)
 app=Flask(__name__)
 
 @app.route('/')
@@ -8,4 +11,8 @@ def home():
     return  render_template('index.html')
 
 if __name__=='__main__':
+<<<<<<< HEAD
  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+=======
+    app.run(debug=True,port=5050)
+>>>>>>> 91bf385 (updating resume)
